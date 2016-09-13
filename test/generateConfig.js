1cbce6f37d923e3f1fe03da77aab27e950a64f1e
Jorrit Poelen <jhpoelen@gmail.com>
2016-09-12 18:51:21 -0700

    download dynamic osmose configuration instead of dynamic one; introduce timestep, see https://github.com/jhpoelen/fb-osmose-bridge/issues/62

--- test/generateConfig.js
+++ test/generateConfig.js
@@ -4,14 +4,23 @@ var test = require('tape');
 test('valid request', function (t) {
     t.plan(2);
 
-    var callback = function (err, resp, body) {
+    var callback = function (err, url) {
         console.log(err);
         t.notOk(err instanceof Error);
-        t.equals(resp.statusCode, 200, "expected status code 200 (OK)");
-        window.location = "https://fbob.herokuapp.com/osmose_config.zip";
+        t.ok(url.indexOf("data:") > -1);
     };
 
-    osmose.generateConfig(functionalGroups, callback);
+    osmose.generateConfig(validConfig, callback);
+});
+
+test('no longer valid request', function (t) {
+    t.plan(1);
+
+    var callback = function (err, resp, body) {
+        t.ok(err instanceof Error);
+    };
+
+    osmose.generateConfig(groupsOnly, callback);
 });
 
 test('invalid request', function (t) {
@@ -21,29 +30,33 @@ test('invalid request', function (t) {
         t.ok(err instanceof Error);
     };
 
-    osmose.generateConfig({ invalid: true }, callback);
+    osmose.generateConfig({ donald: "duck" }, callback);
 });
 
 
-var functionalGroups = [
+var groupsOnly = [
     {
-        "name": "Amberjacks",
-        "type": "focal",
-        "taxa": [
+        name: "Amberjacks",
+        type: "focal",
+        taxa: [
             {
-                "name": "Seriola dumerili",
-                "url": "http://fishbase.org/summary/1005"
+                name: "Seriola dumerili",
+                url: "http://fishbase.org/summary/1005"
             }
         ]
     },
     {
-        "name": "Echinoderms and large gastropods",
-        "type": "background",
-        "taxa": [
+        name: "Echinoderms and large gastropods",
+        type: "background",
+        taxa: [
             {
-                "name": "Echinus esculentus",
-                "url": "http://sealifebase.org/summary/49996"
+                name: "Echinus esculentus",
+                url: "http://sealifebase.org/summary/49996"
             }
         ]
     }
 ];
+var validConfig = {
+    timeStepsPerYear: 123,
+    groups: groupsOnly
+};
