117d3c48547edd8ae06c4555170d0d8f2ac4ac20
Jorrit Poelen <jhpoelen@gmail.com>
2016-09-16 12:52:05 -0700

    validate that configuration include at least one focal group and one background group, related to #68. @FIN-casey please upgrade the javascript bridge library

--- test/generateConfig.js
+++ test/generateConfig.js
@@ -5,7 +5,6 @@ test('valid request', function (t) {
     t.plan(2);
 
     var callback = function (err, url) {
-        console.log(err);
         t.notOk(err instanceof Error);
         t.ok(url.indexOf("data:") > -1);
     };
@@ -33,28 +32,53 @@ test('invalid request', function (t) {
     osmose.generateConfig({ donald: "duck" }, callback);
 });
 
+test('invalid request: no background groups', function (t) {
+    t.plan(1);
 
+    var callback = function (err, resp, body) {
+        t.ok(err instanceof Error);
+    };
+
+
+    osmose.generateConfig({ timeStepsPerYear: 123, groups: [focalGroup]}, callback);
+});
+
+test('invalid request: no focal groups', function (t) {
+    t.plan(1);
+
+    var callback = function (err, resp, body) {
+        t.ok(err instanceof Error);
+    };
+
+
+    osmose.generateConfig({ timeStepsPerYear: 123, groups: [backgroundGroup]}, callback);
+});
+
+
+var backgroundGroup = {
+    name: "Echinoderms and large gastropods",
+    type: "background",
+    taxa: [
+        {
+            name: "Echinus esculentus",
+            url: "http://sealifebase.org/summary/49996"
+        }
+    ]
+};
+
+var focalGroup = {
+    name: "Amberjacks",
+    type: "focal",
+    taxa: [
+        {
+            name: "Seriola dumerili",
+            url: "http://fishbase.org/summary/1005"
+        }
+    ]
+};
 var groupsOnly = [
-    {
-        name: "Amberjacks",
-        type: "focal",
-        taxa: [
-            {
-                name: "Seriola dumerili",
-                url: "http://fishbase.org/summary/1005"
-            }
-        ]
-    },
-    {
-        name: "Echinoderms and large gastropods",
-        type: "background",
-        taxa: [
-            {
-                name: "Echinus esculentus",
-                url: "http://sealifebase.org/summary/49996"
-            }
-        ]
-    }
+    focalGroup,
+    backgroundGroup
 ];
 var validConfig = {
     timeStepsPerYear: 123,
